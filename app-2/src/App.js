fa742461b17d8f94a18e74bad6b445e9363435c4
MikeRowManage <michael.priestley88@yahoo.com>
2020-01-29 16:13:16 -0700

    App 2 push

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      itemList: ["Eggs", "Bacon", "Toast"]
+      
+    }
+
+  }
+
+
+render() { 
+    let itemsToDisplay = this.state.itemList.map((element, index) => {
+      return <h2 key={index}>{element}</h2>
+      })
+  
+return <div className="App">{itemsToDisplay}</div>
 }
 
-export default App;
+}
+
+export default App
