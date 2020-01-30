8b5fd3f7edf45b72b75917225ef6ed45f7b43dfa
MikeRowManage <michael.priestley88@yahoo.com>
2020-01-29 17:50:31 -0700

    App 3 push

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+  
+  this.state = {
+    foodList: ["milk", "eggs", "cheese", "bread"],
+    userInput: ''
+  }
+
+  
+  }
+  handleChange = (val) => {
+    this.setState ({
+      userInput: val
+    })
+  }
+
+  render() {
+    let foodListDisplay = this.state.foodList
+      .filter((element, index) => {
+        return element.includes(this.state.userInput);
+      })
+      .map((element, index) => {
+        return <h2 key={index}>{element}</h2>;
+      })
+  return <div className='App'>
+              <input onChange={(e) => this.handleChange(e.target.value)} type="text"/>
+              {foodListDisplay}</div>
+  }
 }
+
 
 export default App;
